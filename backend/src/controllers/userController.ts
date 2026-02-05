58d0780bee663f4fe86631d5dc3118118e8b4c42
arbabprvt-ofc <arbabprvt@gmail.com>
2026-02-05 20:52:31 +0530

    Add password update controller

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { getProfile, updateProfile } from "../services/userService";
+import { getProfile, updatePassword, updateProfile } from "../services/userService";
 import { successResponse } from "../utils/responses";
 
 export const getMeHandler = async (
@@ -39,6 +39,28 @@ export const updateMeHandler = async (
 
     const updated = await updateProfile(userId, req.body);
     successResponse(res, updated, "Profile updated");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updatePasswordHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const userId = req.user?.id;
+
+    if (!userId) {
+      const error = new Error("Unauthorized");
+      (error as Error & { statusCode?: number }).statusCode = 401;
+      throw error;
+    }
+
+    const { currentPassword, newPassword } = req.body;
+    await updatePassword(userId, currentPassword, newPassword);
+    successResponse(res, { updated: true }, "Password updated");
   } catch (error) {
     next(error);
   }
