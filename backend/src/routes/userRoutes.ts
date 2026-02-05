40e4e568a4ef3662e21b33b52877ec4a465bb3b9
arbabprvt-ofc <arbabprvt@gmail.com>
2026-02-05 20:53:11 +0530

    Add password update route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { getMeHandler, updateMeHandler } from "../controllers/userController";
+import { getMeHandler, updateMeHandler, updatePasswordHandler } from "../controllers/userController";
 import { requireAuth } from "../middlewares/auth";
 import { validateRequest } from "../middlewares/validation";
-import { updateProfileSchema } from "../validators/userValidator";
+import { updatePasswordSchema, updateProfileSchema } from "../validators/userValidator";
 
 const router = Router();
 
 router.get("/me", requireAuth, getMeHandler);
 router.put("/me", requireAuth, validateRequest(updateProfileSchema), updateMeHandler);
+router.put("/me/password", requireAuth, validateRequest(updatePasswordSchema), updatePasswordHandler);
 
 export default router;
