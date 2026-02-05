bb9a84c226fcac4c7b8de99a86c80fffa875dc83
arbabprvt-ofc <arbabprvt@gmail.com>
2026-02-05 20:26:02 +0530

    Remove sidebar from layout

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
 import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
     <div className="min-h-screen bg-[#cfc3f4]">
-      <Navbar
-        onMenuToggle={() => setSidebarOpen((prev) => !prev)}
-        isMenuOpen={sidebarOpen}
-      />
-      <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-      <main className="px-4 pb-16 pt-6 lg:ml-64 lg:px-10">{children}</main>
+      <Navbar />
+      <main className="px-4 pb-16 pt-6 lg:px-10">{children}</main>
       <Footer />
     </div>
   );
